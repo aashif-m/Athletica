3cb63ed7b8e5fcc24e805817c2adaae8708601cd
Chenuka608 <chenuka478@gmail.com>
2023-07-05 08:48:51 +0530

    BG changing button added

---
+++ Gallery/coursework.js
@@ -0,0 +1,12 @@
+function change() {
+    var body = document.body;
+    
+    if (body.style.backgroundColor === 'black') {
+      body.style.backgroundColor = 'white';
+      body.style.color = 'black';
+    } else {
+      body.style.backgroundColor = 'black';
+      body.style.color = 'white';
+    }
+  }
+  
