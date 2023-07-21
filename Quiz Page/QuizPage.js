4f242c35021b805422865c6b7b8d132060310e4e
Pragash Sasitharan <Pragash1001@gmail.com>
2023-07-21 12:05:20 +0530

    adds changes

--- Quiz Page/QuizPage.js
+++ Quiz Page/QuizPage.js
@@ -1,8 +1,3 @@
-const start_btn
-
-
-
-
 const questions = [
     {
         question: "Which athlete is often referred to as 'The Flying Sikh' and represented India in the 1960 and 1964 Summer Olympics?",
@@ -96,6 +91,16 @@ const questions = [
 ]
 
 let randomquestions = []
+let secondsleft = 120;
+let index = 0;
+let QuestionNumber = 1;
+let Score = 0;
+
+const timelefttext = document.querySelector(".timer .time_left_text")
+const timeleftcount = document.querySelector(".timer .time_left")
+const timeLeftText = document.querySelector(".timer .time_left_text");
+const timeLeftCount = document.querySelector(".timer .time_left");
+const nextBtn = document.querySelector(".next_question_btn");
 
 function randomisequestions(){
     while (randomquestions.length <=9){
@@ -103,3 +108,104 @@ function randomisequestions(){
         if(!randomquestions.includes(random)) randomquestions.push(random)
     }
 }
+
+const timer = setInterval(timer, 1000);
+function timer(){
+    document.getElementById("time_left").textContent = secondsleft;
+    secondsleft--;
+    if(secondsleft === 0){
+        clearInterval(timer);
+        alert("Time is UP!!")
+        ShowResults()
+    }
+}
+
+function NextQuestion(index){
+    randomisequestions()
+    const activequestion = randomquestions[index];
+    document.getElementById("question_number").innerHTML = QuestionNumber
+    document.getElementById("display-question").innerHTML = activequestion.question;
+    document.getElementById("option-A").innerHTML = activequestion.optionA;
+    document.getElementById("option-B").innerHTML = activequestion.optionB;
+    document.getElementById("option-C").innerHTML = activequestion.optionC;
+    document.getElementById("option-D").innerHTML = activequestion.optionD;
+}
+
+function CheckAnswer(){
+    const activequestion = randomquestions[index]
+    const CorrectAnswer = activequestion.correctoption
+    const alloptions = document.getElementsByName("option");
+    let correctoption = null
+
+    alloptions.forEach((opt) => {
+        if(opt.value === CorrectAnswer){
+            correctoption = opt.labels[0].id
+        }
+    })
+
+    options.forEach((opt) => {
+        if (opt.checked === true && opt.value === correctoption) {
+            document.getElementById(correctoption).style.backgroundColor = "#3ee739"
+            Score++
+            index++
+        }
+
+        else if (opt.checked && opt.value !== CorrectAnswer) {
+            const wrongLabelId = opt.labels[0].id
+            document.getElementById(wrongLabelId).style.backgroundColor = "#ff5858"
+            document.getElementById(correctoption).style.backgroundColor = "#3ee739"
+            index++
+        }
+    })
+}
+
+function ButtonNextQuestion(){
+    CheckAnswer()
+
+    const opt = document.getElementsByName("option");
+    for (let i = 0; i < opt.length; i++) {
+        opt[i].checked = false;
+    }
+
+    if (index < 10) {
+        NextQuestion(index)
+        ResetOptionColour()
+    }
+    else {
+        ShowResults()
+    }
+}
+
+function ResetOptionColour() {
+    const options = document.getElementsByName("option");
+    options.forEach((option) => {
+        document.getElementById(option.labels[0].id).style.backgroundColor = ""
+    })
+}
+
+function ShowResults(){
+    let remark = null
+    let remarkcolor = null
+
+    if(Score > 7){
+        remark = "Excellent 🥳, Keep working hard!!"
+        remarkcolor = "green"
+    } else if (Score > 4){
+        remark = "Good 🙌, Can do better!!"
+        remarkcolor = "orange"
+    } else {
+        remark="Missed it 😔, Keep Practicing and better luck!!"
+        remarkcolor = "red"
+    }
+}
+
+// Event listener for the "Next Question" button
+nextBtn.addEventListener("click", nextQuestion);
+
+// Start the quiz when the page loads
+document.addEventListener("DOMContentLoaded", () => {
+  randomizeQuestions();
+  showNextQuestion();
+  startTimer();
+});
+
