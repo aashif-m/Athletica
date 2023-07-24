4884102273520375c7880888723b9133c18f34fd
Muhammadu Ishrath <ishrathmuhammadu@gmail.com>
2023-07-24 14:36:55 +0530

    2.360i

--- Main Page/Main.js
+++ Main Page/Main.js
@@ -1,11 +1,22 @@
-const scroll_down = document.querySelector(".main-button");
+    
+    
+    // Select the element with the class "main-button"
+    // and assign  it to a variable called "scroll_down".
+    const scroll_down = document.querySelector(".main-button");
 
-scroll_down.onclick = ()=>{
-    window.scrollTo(0,1000);
-}
+    // Attach a click event handler to the "scroll_down" element
+    scroll_down.onclick = ()=>{
+        //scroll the window 1000px vertically down.
+        window.scrollTo(0,1000);
+    }
 
-const scroll_up = document.querySelector("#back-to-top-btn");
+    // Select the element with the id "back-to-top-btn" and  
+    //assign  it to a variable called "scroll_up".
+    const scroll_up = document.querySelector("#back-to-top-btn");
 
-scroll_up.onclick = ()=>{
-    window.scrollTo(0,0);
-}
+    // Attach a click event handler to the "scroll_up" element
+    scroll_up.onclick = ()=>{
+        //scroll the window to the top-left most position of the page.
+        window.scrollTo(0,0);
+    }
+
